07370090003f8f51d21765e8cf7cea02eae0b74b
Uexcel2022 <121309125+Uexcel2022@users.noreply.github.com>
2023-10-20 15:50:30 +0100

    commit

--- src/main/java/com/uexcel/controller/DepartmentController.java
+++ src/main/java/com/uexcel/controller/DepartmentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,4 +39,10 @@ public class DepartmentController {
     public void deleteDepartment(@PathVariable Long id) {
         departmentService.deleteDepartmentById(id);
     }
+
+    @PutMapping("/departments")
+    public Department putDepartment(@RequestBody Department department) {
+        return departmentService.updateDepartment(department);
+    }
+
 }
