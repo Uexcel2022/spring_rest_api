4c05460fdab4e6158547d6a8228b6f206f2af7c8
Uexcel2022 <excellenceudoka@gmail.com>
2023-10-29 10:25:29 +0100

    comit

--- src/test/java/com/uexcel/service/DepartmentServiceTest.java
+++ src/test/java/com/uexcel/service/DepartmentServiceTest.java
@@ -2,7 +2,7 @@ package com.uexcel.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterAll;
 // import org.junit.jupiter.api.AfterEach;
 // import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
